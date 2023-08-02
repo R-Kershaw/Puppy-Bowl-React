dedad1e6628ed72e74620060a5cffb55db152769
R-Kershaw <rkershaw90@gmail.com>
2023-08-02 17:14:27 -0400

    Added AllPlayers functionality. Removed index.js code

--- src/API/index.js
+++
@@ -1,9 +0,0 @@
-const cohortName = '2302-ACC-PT-WEB-PT-A';
-const APIURL = `https://fsa-puppy-bowl.herokuapp.com/api/${cohortName}/`;
-
-module.exports = {
-    cohortName,
-    APIURL,
-};
-
-//Import to other files by using const {} = require(./API/index.js)
--- src/components/AllPlayers.jsx
+++ src/components/AllPlayers.jsx
@@ -1,7 +1,46 @@
+import { useState } from "react";
+import { useEffect } from "react";
+
+const cohortName = '2302-ACC-PT-WEB-PT-A';
+const APIURL = `https://fsa-puppy-bowl.herokuapp.com/api/${cohortName}/`;
+
 export default function AllPlayers() {
-    return (
-        <>
-            <h1>All Players</h1>
-        </>
-    )
+
+    const [players, setPlayers] = useState([]);
+
+
+    useEffect(() => {
+        async function fetchAllPlayers() {
+            try {
+                const response = await fetch(`${APIURL}players`);
+                const result = await response.json();
+                setPlayers(result.data.players);
+                console.log(players);
+            } catch (error) {
+                console.log(error); //should create an error var with useState but not required so w/e
+            }
+        }
+        fetchAllPlayers();
+    }, [])
+
+    if (players) {
+        return (
+            <div>
+                {
+                    players.map((player) => {
+                        return (
+                            <div key={player.id}>
+                                <p>{player.name}</p>
+                            </div>
+                        )
+                    })
+                }
+            </div>
+        )
+        console.log(players);
+    }
+    else {
+        return null;
+    }
+
 }
