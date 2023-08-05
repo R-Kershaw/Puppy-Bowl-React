98da6574d709f8fe58378b4663c785d793cc73f5
Dion Charles <dionr_charles@hotmail.com>
2023-08-05 15:33:08 -0400

    one change

--- src/main.jsx
+++ src/main.jsx
@@ -2,10 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import App from './App.jsx';
-import AllPlayers from './AllPlayers.jsx';
-import SinglePlayer from './SinglePlayer.jsx';
-import NewPlayerForm from './NewPlayerForm.jsx';
-import { fetchAllPlayers, fetchSinglePlayer } from './fetchPlayers'; // Correct the import path for fetchAllPlayers and fetchSinglePlayer
+import AllPlayers from './components/AllPlayers.jsx';
+import SinglePlayer from './components/SinglePlayer.jsx';
+import NewPlayerForm from './components/NewPlayerForm.jsx';
+import { fetchAllPlayers, fetchSinglePlayer } from './components/fetchPlayers.js'; // Correct the import path for fetchAllPlayers and fetchSinglePlayer
 
 const init = async () => {
   const players = await fetchAllPlayers();
