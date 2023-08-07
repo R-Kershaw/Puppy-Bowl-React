84efafd5031d25df7b787762f692137b36c98623
R-Kershaw <rkershaw90@gmail.com>
2023-08-07 14:50:47 -0400

    renamed merge Navbar component to NavBar

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,9 @@ import './App.css'
 import AllPlayers from './components/AllPlayers.jsx';
 import SinglePlayer from './components/SinglePlayer.jsx';
 import NewPlayerForm from "./components/NewPlayerForm";
-import Navbar from './components/Navbar';
+import NavBar from './components/NavBar';
 import { Route, Routes } from 'react-router-dom';
+
 
 export default function App() {
   const cohortName = '2302-ACC-PT-WEB-PT-A';
