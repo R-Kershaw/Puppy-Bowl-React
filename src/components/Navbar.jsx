f896e6857325c08abca92bafb5ddf4547dcf0b03
R-Kershaw <rkershaw90@gmail.com>
2023-08-07 14:41:56 -0400

    fixed messed up navBar Component from merging

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,16 +1,9 @@
 import { Link } from "react-router-dom";
-<<<<<<< HEAD
-export default function Navbar() {
-    return (
-        <nav>
-            <Link to="/home">Home</Link>
-=======
 
 export default function NavBar() {
     return (
         <nav>
             <Link to="/">Home</Link>
->>>>>>> origin/rk
             <Link to="/players">All Players</Link>
             <Link to="/newPlayer">Add Player</Link>
         </nav>
